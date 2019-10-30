0ca266e6b11ee9dec2239753c1ca748141bdbb7f
JiangWeixian <saber_wei@163.com>
2019-10-30 19:44:17 +0800

    feat: first version

--- example/src/epics/sharks.ts
+++ example/src/epics/sharks.ts
@@ -1,11 +1,11 @@
 import { Epic } from 'redux-observable'
-import { filter } from 'rxjs/operators'
+import { filter, mapTo } from 'rxjs/operators'
 
 export const sharkEpics: Epic<any, any, any> = (action$, state$) => {
   return action$.pipe(
     filter(v => {
-      console.log(v)
-      return v.type === 'dolphins'
+      return v.type === 'dolphins/increment'
     }),
+    mapTo({ type: 'sharks/increment', payload: 1 }),
   )
 }
--- example/src/store.ts
+++ example/src/store.ts
@@ -1,32 +1,34 @@
 import { init, RematchRootState, RematchDispatch } from '@rematch/core'
-import { combineEpics, createEpicMiddleware } from 'redux-observable'
-import { compose, createStore, applyMiddleware } from 'redux'
+// import { combineEpics, createEpicMiddleware } from 'redux-observable'
+// import { compose, createStore, applyMiddleware } from 'redux'
 
 import * as models from './models'
 import { sharkEpics } from './epics/sharks'
 
-const epics = combineEpics(...[sharkEpics])
-const epicsMiddware = createEpicMiddleware()
+// const epics = combineEpics(...[sharkEpics])
+// const epicsMiddware = createEpicMiddleware()
 
-const newCreateStore: any = (reducer, initalState, enhancer) => {
-  return createStore(
-    reducer,
-    initalState,
-    compose(
-      applyMiddleware(...[epicsMiddware]),
-      enhancer,
-    ),
-  )
-}
+// const newCreateStore: any = (reducer, initalState, enhancer) => {
+//   return createStore(
+//     reducer,
+//     initalState,
+//     compose(
+//       applyMiddleware(...[epicsMiddware]),
+//       enhancer,
+//     ),
+//   )
+// }
+
+import createRematchObservable from 'rematch-observable'
+
+const rematchObservable = createRematchObservable({ epics: { sharkEpics } })
 
 export const store = init({
   models,
-  redux: {
-    createStore: newCreateStore,
-  },
+  plugins: [rematchObservable],
 })
 
-epicsMiddware.run(epics)
+// epicsMiddware.run(epics)
 
 export type Store = typeof store
 export type Dispatch = RematchDispatch<typeof models>
